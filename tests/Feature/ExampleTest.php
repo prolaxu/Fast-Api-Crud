5e5d02fedff4d2cf0398c5db164b63a8a7572e37
anil kumar thakur <anilkumarthakur60@gmail.com>
2024-03-31 18:34:24 +0545

    fix:styleci

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,7 @@
 <?php
 
 describe(description: 'feature test', tests: function () {
-    it('example', function () {
+    it(description: 'example', closure: function () {
         expect(true)->toBeTrue();
     });
 });
